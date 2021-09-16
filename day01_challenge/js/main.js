d039c12218c77d625fc53afec7dafc2700e75841
jpoliveira08 <joaopedro.oliveirajava@gmail.com>
2021-09-15 22:58:28 -0300

    Add desafio day 01

---
+++ day01_challenge/js/main.js
@@ -0,0 +1,7 @@
+var tempCelsius = prompt("Digite a temperatura em grau Celsius °C");
+var constA = 9.0/5.0;
+var constB = 32;
+
+var tempFahren = (tempCelsius * constA) + constB;
+
+alert("A temperatura equivalente em Fahrenheit é: " + tempFahren + " °F");
